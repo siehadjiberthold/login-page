aa269f0e5b1f852ba094db045c9c9809f3052532
siehadjiberthold <109277481+siehadjiberthold@users.noreply.github.com>
2022-11-10 13:32:13 +0100

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,57 +4,7 @@ import ContactForm from './component/ContactForm';
 export default function App() {
   return (
     <div className="bodyform">
-      <div className="part contactform">
-        <h1> Welcome Back</h1>
-        <input type="text" placeholder=" login with Google" />
-        <hr /> OK LOGIN WITH EMAIL
-        <div>
-          <form action="">
-            <label htmlFor="">
-              Email Adress
-              <input type="text" placeholder="Email Adress" />
-            </label>
-            <label htmlFor="">
-              Password
-              <input type="text" placeholder="password" />
-            </label>
-            <label>
-              <input type="checkbox" />
-              keep me logged in
-            </label>
-            <label>
-              <input type="text" />
-              <a href=""> forget your passeword?</a>
-            </label>
-          </form>
-          <button onclick="submit()">Log in</button>
-          <p>
-            Don't have an account?
-            <a href=""> sign in</a>
-          </p>
-        </div>
-      </div>
-
-      <div
-        className=" part academy"
-        style={{
-          backgroundImage:
-            'url("https://media.istockphoto.com/vectors/illustration-of-a-group-of-people-eating-outdoors-at-a-restaurant-vector-id1285195940?k=20&m=1285195940&s=612x612&w=0&h=rtDPS2a4dR-Z4m9JGaTpOyaWE1V8IXEmiVHwjtF3PGI= ")',
-        }}
-      >
-        <div style={{ width: 300 }}>
-          <p style={{ fontSize: 18, fontWeight: '700', marginBottom: 16 }}>
-            {' '}
-            Sleeknote Academy
-          </p>
-          <p>
-            {' '}
-            bienvenur dans sleenote academy <br /> veuillez renseigner vos
-            information pour vous connecter a notre site{' '}
-          </p>
-          <button onclick="submit()">START ACADEMY</button>
-        </div>
-      </div>
+      <ContactForm/>
     </div>
   );
 }
--- src/component/ContactForm.js
+++ src/component/ContactForm.js
@@ -2,31 +2,65 @@ import React from 'react';
  import ContactForm from './component/ContactForm'
 function ContactForm() {
   return (
-    <div className='contact'>
+    <div className="bodyform">
+    <div className="part contactform">
+      <div>
+      Sleeknote
+      </div>
       <h1> Welcome Back</h1>
-      <form>
-        <input type="text"  placeholder= "log in with google" />
- <h3>ok login with your email</h3>
-        <label for="surname">Email Adress</label>
-        <input type="email" id="surname" placeholder=" Email Adress" />
-
-        <label for="password">password</label>
-        <input type="password" id="password" placeholder=" password" />
+      <input type="text" placeholder=" login with Google" />
+      <hr /> OK LOGIN WITH EMAIL
+      <div>
+        <form action="">
+          <div>
+          <label htmlFor="">
+            Email Adress
+          </label>
+          <input type="text" placeholder="Email Adress" />
+          </div>
+         <div>
+         <label htmlFor="">
+            Password
+          </label>
+          <input type="text" placeholder="password" />
+         </div>
+        
+        </form>
         <label>
-        <input type="checkbox"/>
-        keep me logged in
-        </label>
-        <label>
-        <input type="text"/>
-      <a href=""> forget your passeword?</a>  
-        </label>
-      </form>
-      <button onclick="submit()">Log in</button>
-      <p> 
-        Don't have an account?
-        <a href=""> sign in</a>
-      </p>
+            <input type="checkbox" />
+            keep me logged in
+            <a href=""> forget your passeword?</a>
+          </label>
+        
+        <button onclick="submit()">Log in</button>
+        <p>
+          Don't have an account?
+          <a href=""> sign in</a>
+        </p>
+      </div>
     </div>
+
+    <div
+      className=" part academy"
+      style={{
+        backgroundImage:
+          'url("https://media.istockphoto.com/vectors/illustration-of-a-group-of-people-eating-outdoors-at-a-restaurant-vector-id1285195940?k=20&m=1285195940&s=612x612&w=0&h=rtDPS2a4dR-Z4m9JGaTpOyaWE1V8IXEmiVHwjtF3PGI= ")',
+      }}
+    >
+      <div  className='academyblock'   style={{ width: 300 }}>
+        <p style={{ fontSize: 18, fontWeight: '700', marginBottom: 16 }}>
+          {' '}
+          Sleeknote Academy
+        </p>
+        <p>
+          {' '}
+          bienvenur dans sleenote academy <br /> veuillez renseigner vos
+          information pour vous connecter a notre site{' '}
+        </p>
+        <button onclick="submit()">START ACADEMY</button>
+      </div>
+    </div>
+  </div>
   );
 }
 export default ContactForm;
