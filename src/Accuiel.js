53ae05983da4719a68b4b3a33a13eb82dcff2087
siehadjiberthold <109277481+siehadjiberthold@users.noreply.github.com>
2022-11-11 17:51:31 +0100

    1 file modified

--- src/Accuiel.js
+++ src/Accuiel.js
@@ -1,8 +1,15 @@
-import React from 'react'
-export default function Accuiel(){
-  return(
-    <div>
-      Bonjour 
-    </div>
-  )
+import React, { useContext, useState, useEffect } from 'react';
+export default function Accuiel() {
+  const [storage, setStorage] = useState({});
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const appStorage = window.localStorage.getItem('appStorage');
+      const JsonEncoder = JSON.parse(appStorage);
+      console.log('encoder', JsonEncoder);
+      setStorage(JsonEncoder);
+    }
+  }, []);
+  console.log(storage);
+  return <div>Bonjour {storage.email}</div>;
 }
